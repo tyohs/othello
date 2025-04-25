98b8be6e72d49610b08225a72bf7dcfeb59a8c2e
tyohs <gamhpzo@gmail.com>
2025-04-26 03:11:37 +0900

    'かっこの範囲に注意して.'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,11 @@ export default function Home() {
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    newBoard[y][x] = turnColor;
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
+      setBoard(newBoard);
     }
-    setBoard(newBoard);
   };
   return (
     <div className={styles.container}>
